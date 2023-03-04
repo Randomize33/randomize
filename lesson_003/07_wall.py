a9445e462efed5bae1a25038dd4498660abc4b7b
Randomize <test@test.ru>
2023-03-04 12:32:21 +0300

    Задание_07_wall

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -6,6 +6,23 @@ import simple_draw
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+
+for y in range (0,700,100):
+    start_point = simple_draw.get_point(0, y)
+    end_point = simple_draw.get_point(600, y)
+    simple_draw.line(start_point=start_point,end_point=end_point)
+    for x in range (100,600,100):
+        start_point = simple_draw.get_point(x, y)
+        end_point = simple_draw.get_point(x, y+50)
+        simple_draw.line(start_point=start_point, end_point=end_point)
+
+for y in range (50,700,100):
+    start_point = simple_draw.get_point(0, y)
+    end_point = simple_draw.get_point(600, y)
+    simple_draw.line(start_point=start_point,end_point=end_point)
+    for x in range (50,600,100):
+        start_point = simple_draw.get_point(x, y)
+        end_point = simple_draw.get_point(x, y+50)
+        simple_draw.line(start_point=start_point, end_point=end_point)
 
 simple_draw.pause()
