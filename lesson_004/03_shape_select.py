6e6700925997c9af9f7f8b0203c62a6f2c64ced6
Randomize <test@test.ru>
2023-03-15 22:34:59 +0300

    Задание_03_shape_select

--- lesson_004/03_shape_select.py
+++ lesson_004/03_shape_select.py
@@ -9,6 +9,23 @@ import simple_draw as sd
 # Код функций из упр lesson_004/02_global_color.py скопировать сюда
 # Результат решения см lesson_004/results/exercise_03_shape_select.jpg
 
-# TODO здесь ваш код
+def draw_figure(start_point,angle,length):
+    if angle == 360:
+        pass
+    else:
+        vector=sd.vector(start_point,angle,length)
+        start_point=vector
+        draw_figure(start_point,angle+angle_delta,length)
+
+figure_list=[3, 4, 5, 6]
+print('''
+1. Треугольник
+2. Квадрат
+3. Пятиугольник
+4. Шестиугольник
+''')
+figure_number = int(input("Введите номер фигуры: "))
+angle_delta = 360/int(figure_list[figure_number-1])
+draw_figure(sd.get_point(250,250),0,100)
 
 sd.pause()
