ee1c373a2043cb473b37034ce8cf45ad16e738ee
Randomize <test@test.ru>
2023-04-19 22:00:58 +0300

    Задание_009 Парсер логов

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 # Имеется файл events.txt вида:
 #
 # [2018-05-17 01:55:52.665804] NOK
@@ -19,7 +20,35 @@
 # Входные параметры: файл для анализа, файл результата
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+class Parser():
+
+    def __init__(self, file_in, find_str, file_out):
+        self.file_in=file_in
+        self.find_str=find_str
+        self.file_out=file_out
+        self.result={}
+    def count_value(self):
+        with open(self.file_in, mode='r', encoding='utf8') as file:
+            for line in file:
+                if self.find_str in line:
+                    if line[1:17] in self.result:
+                        self.result[line[1:17]]+=1
+                    else:
+                        self.result[line[1:17]]=1
+        self.write_to_file()
+
+    def write_to_file(self):
+        with open(self.file_out, mode='w') as file:
+            for date in self.result:
+                file.write('[{}] {} \n'.format(date,self.result.get(date)))
+
+
+collect_data=Parser('events.txt',' OK','log_result.txt')
+collect_data.count_value()
+collect_data.write_to_file()
+
+
+
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
