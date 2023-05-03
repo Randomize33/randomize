5a20dafb759310a5becf41d83a8162eb107b1f70
Randomize <test@test.ru>
2023-05-03 21:24:20 +0300

    Задание_010 Задание_01

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -4,11 +4,17 @@
 
 BRUCE_WILLIS = 42
 
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
-
+try:
+    input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+except ValueError:
+    print("Невозможно преобразовать к числу")
+except IndexError:
+    print("Вы ввели слишком маленькое число")
+except:
+    print("Непредвиденая ошибка")
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
 # - IndexError - выход за границы списка
