06116c7635adf5ac05bd1ad1224624011f7e28f6
Randomize <test@test.ru>
2023-05-04 23:14:01 +0300

    Задание_010 02_День сурка

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import random
 # День сурка
 #
 # Напишите функцию one_day() которая возвращает количество кармы от 1 до 7
@@ -19,6 +20,43 @@
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
 
-# TODO здесь ваш код
+class IamGodError(Exception):
+    pass
+class DrunkError(Exception):
+    pass
+class CarCrashError(Exception):
+    pass
+class GluttonyError(Exception):
+    pass
+class DepressionError(Exception):
+    pass
+class SuicideError(Exception):
+    pass
+
+def groundhog_day():
+    karma=0
+    while karma<ENLIGHTENMENT_CARMA_LEVEL:
+        try:
+            rnd=random.randint(1,13)
+            if rnd == 13:
+                error=random.randint(1,6)
+                if error==1:
+                    raise IamGodError("Я не Бог!")
+                elif error==2:
+                    raise DrunkError("Напился")
+                elif error==3:
+                    raise CarCrashError("Разбился")
+                elif error==4:
+                    raise GluttonyError("Объелся")
+                elif error==5:
+                    raise DepressionError("Приуныл")
+                elif error==6:
+                    raise SuicideError("Самоубился")
+        except Exception as exc:
+            print(f"{exc}")
+        finally:
+            karma+=random.randint(1,7)
+    print(karma)
+groundhog_day()
 
 # https://goo.gl/JnsDqu
