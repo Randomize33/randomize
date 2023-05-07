830eb45afbc12b85aea2f95654a09278e5bfafe7
Randomize <test@test.ru>
2023-05-07 21:41:34 +0300

    Задание_011 Урок3_parser

--- lesson_011/03_log_parser.py
+++ lesson_011/03_log_parser.py
@@ -14,4 +14,24 @@
 #
 # [2018-05-17 01:57] 1234
 
-# TODO здесь ваш код
+def log_parser(file,find_str):
+    prew_line=""
+    with open(file, mode='r', encoding='utf8') as file:
+        for line in file:
+            if find_str in line:
+                if line[1:17] in result:
+                    result[line[1:17]] += 1
+                else:
+                    try:
+                        result[line[1:17]] = 1
+                        yield (prew_line,result[prew_line])
+                        prew_line = line[1:17]
+                    except KeyError:
+                        prew_line = line[1:17]
+
+file="events.txt"
+find_str="NOK"
+result = {}
+
+for line in log_parser(file,find_str):
+    print(line)
