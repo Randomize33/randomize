672a9e180af26ae6205b9801ee330cd63180889b
Randomize <test@test.ru>
2023-05-09 11:51:42 +0300

    Задание_011 Урок4_декоратор

--- lesson_011/04_error_log_decorator.py
+++ lesson_011/04_error_log_decorator.py
@@ -8,24 +8,26 @@
 # Лог файл открывать каждый раз при ошибке в режиме 'a'
 
 
-def log_errors(func):
-    def wrapper(*args,**kwargs):
-        try:
-            func(args,**kwargs)
-        except Exception as exc:
-            with open("function_errors.log",mode="a",encoding="UTF8") as log_file:
-                log_file.write(f"Функция: {func.__name__} выдала ошибку: {exc} \n")
-    return wrapper
+def log_errors(log):
+    def decorator(func):
+        def wrapper(*args,**kwargs):
+            try:
+                func(args,**kwargs)
+            except Exception as exc:
+                with open(log,mode="a",encoding="UTF8") as log_file:
+                    log_file.write(f"Функция: {func.__name__} выдала ошибку: {exc} \n")
+        return wrapper
+    return decorator
 
 
 
 # Проверить работу на следующих функциях
-@log_errors
+@log_errors(log="function_errors.log")
 def perky(param):
     return param / 0
 
 
-@log_errors
+@log_errors(log="function_errors.log")
 def check_line(line):
     name, email, age = line.split(' ')
     if not name.isalpha():
@@ -44,12 +46,16 @@ lines = [
     'Земфира 86',
     'Равшан wmsuuzsxi@mail.ru 35',
 ]
+
+perky(param=42)
+
 for line in lines:
     try:
         check_line(line)
     except Exception as exc:
         print(f'Invalid format: {exc}')
-perky(param=42)
+
+
 
 
 # Усложненное задание (делать по желанию).
